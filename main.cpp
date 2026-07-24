d7de733c7d996a53616ad2fa197a66626c4ef424
Filip de Meyere <fdemeyere>
2026-07-24 20:50:44 +0200

    put municipal tax data in hashmap

--- main.cpp
+++ main.cpp
@@ -3,48 +3,86 @@
 #include <string>
 #include <sstream>
 #include <vector>
+#include <unordered_map>
+#include <algorithm>
 
 #ifdef _WIN32
 #include <windows.h>
 #endif
 
-void read_municipal_tax_rates(std::string mun_tax_file) {
+bool is_numeric(std::string str) {
+    for (char ch : str) {
+        if (!isdigit(ch) && ch != '-' && ch != ','){
+            return false;
+        }
+    }
+    return true;
+}
+
+std::unordered_map<std::string, std::vector<float>> read_municipal_tax_rates(std::string mun_tax_file) {
 
     std::ifstream file(mun_tax_file);
 
-    if (!file.is_open()) {
+    try {
+        if (!file.is_open()) {
+            throw 505;
+        }
+    } catch (int num) {
         std::cout << "Could not open file: " + mun_tax_file;
-        return;
-    } else {
-        std::cout << "Successfully opened file" << std::endl;
+
     }
 
+    std::unordered_map<std::string, std::vector<float>> mun_tax_rates;
     std::string row;
-    int i = 0;
     while (std::getline (file, row)) {
-        if (i == 2) break;
         std::stringstream ss(row);
 
         std::string token;
+        std::vector<std::string> mun_name_parts;
+        std::vector<float> numerics;
         while (ss >> token) {
-            std::cout << token << std::endl;
+
+            std::string weird_minus = "‑";
+            size_t pos = token.find(weird_minus);
+            if (pos != std::string::npos) {
+                token.replace(pos, weird_minus.length(), "-");
+            }
+
+            std::ranges::replace(token, ',', '.');
+
+            try {
+                float numeric = std::stof(token);
+                numerics.push_back(numeric);
+            } catch (std::exception e) {
+                mun_name_parts.push_back(token);
+            }
         }
 
-        i++;
+        std::string mun_name;
+        for (int i = 0; i < mun_name_parts.size(); i++) {
+            if (i != 0) {
+                mun_name += " ";
+            }
+            mun_name += mun_name_parts[i];
+        }
+
+        mun_tax_rates[mun_name] = numerics;
+
     }
 
-    // Close the file
     file.close();
 
+    return mun_tax_rates;
 }
 
 int main()
 {
     #ifdef _WIN32
-        SetConsoleOutputCP(CP_UTF8);
+    SetConsoleOutputCP(CP_UTF8);
     #endif
 
     std::string mun_tax_file = "municipal_tax_rates.txt";
-    read_municipal_tax_rates(mun_tax_file);
+    std::unordered_map<std::string, std::vector<float>> mun_tax_rates = read_municipal_tax_rates(mun_tax_file);
+    std::cout << mun_tax_rates["Ale"][4];
     return 0;
 }
